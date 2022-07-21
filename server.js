e3c9673a2c7a2682a0a512f004d0d6eb0d8b552b
webdevaadil <75753195+webdevaadil@users.noreply.github.com>
2022-07-21 19:52:51 +0530

    ggg

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 const PORT = process.env.PORT ||5000;
 
-if(process.env.NODE_ENV === 'production'){
-
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./client/build/index.html"))
-  })
+if(process.env.NODE_ENV !== 'production'){
+  require("dotenv").config({ path: "config.env" });
 }
 
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"./client/build/index.html"))
+})
 
 const server=app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
